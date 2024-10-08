74b5961eb251f342ee283a0f9d978258894399ed
Naman Kumar <153828446+namankumar0727@users.noreply.github.com>
2024-10-09 02:03:18 +0530

    Add files via upload

---
+++ Arrays/ArrayObjects.js
@@ -0,0 +1,12 @@
+const a={
+    name:"Naman",
+    10:"10"
+}
+
+console.log(a.name)
+
+console.log(a["name"])
+
+console.log(a.10)
+
+console.log(a["10"])
---
+++ Arrays/Objectspart2.js
@@ -0,0 +1,13 @@
+const a={
+    type:"Car",
+    model:"Bughatti",
+    color:"red"
+}
+
+console.log(typeof (a))
+
+console.log(a.type="Toyota")
+a.Wheels="MRF"
+
+console.log(a)
+
