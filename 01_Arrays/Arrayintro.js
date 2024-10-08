8bacb02faa59e7c96508238b8ba15e7a673cf6f6
Naman Kumar <153828446+namankumar0727@users.noreply.github.com>
2024-10-09 02:04:06 +0530

    Add files via upload

---
+++ 01_Arrays/Arrayintro.js
@@ -0,0 +1,31 @@
+//Creating an empty array
+
+const a=[1,2,23,4];
+
+console.log(a)
+
+//Create array name (favsingers) Store 3 favourite singers
+
+const array=["Jass Manak","Stebin Ben","Shawn Mendes"];
+
+console.log(array[0])
+
+//Create an array and store four favourite numbers
+
+const a1=[1,2,3,4]
+
+console.log(a1)
+
+//Create an array and store one string and an array and a number and a boolean variable
+
+const a2=["akjsnc",["akjncv","lkdsvn"],123,true]
+
+console.log(a2)
+
+//Now access each item by using []
+
+console.log(a2[0])
+console.log(a2[1][0])
+console.log(a2[1][1])
+console.log(a2[2])
+console.log(a2[3])
